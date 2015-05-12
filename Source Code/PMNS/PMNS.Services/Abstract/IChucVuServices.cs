3956cd50616b7ff4a8e0932649670412810c6944
Dung Nguyen Trung <dung.nt100392@gmail.com>
2015-05-12 18:39:27 +0700

    Add ChucVuServices
    Signed-off-by: Dung Nguyen Trung <dung.nt100392@gmail.com>

---
+++ Source Code/PMNS/PMNS.Services/Abstract/IChucVuServices.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMNS.Entities.Models;
+
+namespace PMNS.Services.Abstract
+{
+    interface IChucVuServices
+    {
+    }
+}
